0c5d60c416aa1bbaf0f2d5e0b70f0c6baa88cd91
ProfitBrd <alex.m.deyoung@gmail.com>
2023-07-22 14:00:20 -0400

    Removed headers because not being used

--- client.py
+++ client.py
@@ -4,15 +4,6 @@ from io import BytesIO
 from tabulate import tabulate
 import shlex as shlex
 
-headers = {
-                "Host": "http://192.168.15.25:5000",
-                "Connection": "keep-alive",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-                "Upgrade-Insecure-Requests": "1",
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "en-US,en;q=0.9",
-}
 
 title = '''
  ______   __       ________   ___   __         _______   ______   ______      
